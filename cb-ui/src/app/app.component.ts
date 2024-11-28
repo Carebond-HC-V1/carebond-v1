791e4a030e7b68e8809f2b7d914b36ca63297997
Carebond-HC-V1 <sree.raghuvamshi21@gmail.com>
2024-11-28 22:19:22 +0530

    Add files via upload

--- cb-ui/src/app/app.component.ts
+++ cb-ui/src/app/app.component.ts
@@ -1,25 +1,47 @@
 // app.component.ts
+import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [FormsModule, HttpClientModule],
+  imports: [FormsModule, HttpClientModule, CommonModule],
   
 })
 export class AppComponent {
-  constructor(private http: HttpClient){
+  popupMessage: any;
+  showPopup: boolean = false;
+  constructor(private http: HttpClient) {
 
   }
-  onSubmit(data: any) {
-    console.log(data);
+  // onSubmit(data: any) {
+  //   console.log(data);
     
+  //   this.http.post("/api/send-email", {
+  //     name: "simadri",
+  //     mail: "simadrig75@gmail.com"
+  //   }).subscribe()
+  // }
+
+  onSubmit(form: NgForm) {
+    const { name, email } = form.value; // Fetch form data
     this.http.post("/api/send-email", {
-      name: "simadri",
-      mail: "simadrig75@gmail.com"
-    }).subscribe()
+      name: name,
+      mail: email
+    }).subscribe(()=>{
+      this.popupMessage = `Hello ${name}, someone from our team will shortly contact.`;
+      this.showPopup = true;
+      setTimeout(() => {
+        this.showPopup = false;
+      }, 10000);
+    })
+    form.reset();
+  }
+
+  closePopup(): void{
+    this.showPopup = false;
   }
 }
--- server.js
+++ server.js
@@ -3,27 +3,28 @@ const SibApiV3Sdk = require("sib-api-v3-sdk");
 const app = express();
 app.use(express.json());
 
-const apiKey = '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';
+const apiKey = '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';
 SibApiV3Sdk.ApiClient.instance.authentications["api-key"].apiKey = apiKey;
 app.post("/api/send-email", async (req, res) => {
   const { mail, name } = req.body;
+  console.log(mail, name);
   try {
     const email = new SibApiV3Sdk.TransactionalEmailsApi();
     const sendSmtpEmail = {
-      to: [{ email: mail, name: name }],
+      to: [{ email: "sree.raghuvamshi21@gmail.com", name: "Raghu" }],
       sender: { email: "sree.raghuvamshi21@gmail.com", name: "Sree Raghuvamshi" },
       subject: "subject line testing",
-      htmlContent: `<html><body><p>hello I am working now email is working</p></body></html>`,
+      htmlContent: `<html><body>someone enquired in your site
+      <br/><p>name: ${name}</br> mail: ${mail}</p></body></html>`,
     };
     const response = await email.sendTransacEmail(sendSmtpEmail);
     console.log(response);
     res.status(200).send({ message: "Email sent successfully!", response });
   } catch (error) {
-    console.error(error);
-    res.status(500).send({ message: "Failed to send email", error });
+    res.status(500).send({ message: "Failed to send email"});
   }
 });
-const PORT = process.env.PORT || 3000;
+const PORT = 8081;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
